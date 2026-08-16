d4260f5935495b2dc44f07df6f405a3935921fe6
sweetguma <sooensible@gmail.com>
2026-08-16 17:50:44 +0200

    Feat: add summary curvature

--- src/hVmFigures/infer_pipeline.py
+++ src/hVmFigures/infer_pipeline.py
@@ -1,14 +1,13 @@
 import numpy as np
-import pandas as pd
+from pathlib import Path
 import pickle
-from itertools import permutations
-from hVmFigures.prepro import summary_prepro, reshape_2d_data
-from hVmFigures.config import Nspectra, DATA_DIR, Niter
+from itertools import combinations
+from hVmFigures.config import DATA_DIR, Niter
 from hVmFigures.plotting import sampler_plot
 from hVmFigures.infer import MCMC
 
 
-def inference(S_name_list, niter=6000, plot=True, Npool=False):
+def inference(S_name_list, niter, plot=True, Npool=False):
     Slist = []
     for S_name in S_name_list:
         with open(DATA_DIR / f"summaries/S_{S_name}.pkl", "rb") as f:
@@ -26,10 +25,16 @@ def inference(S_name_list, niter=6000, plot=True, Npool=False):
     return chain
 
 if __name__ == "__main__":
-    S_list = ['fps', 'fpdf', 'sm1', 'sm2', 'lyanna']
-    for S1, S2 in permutations(S_list, 2):
-        chain = inference([S1, S2], niter=Niter)
-        with open(DATA_DIR / "joint_chains" / f"{S1}_{S2}.pkl", "wb") as f:
-            pickle.dump(chain, f)
+    S_list = ['fps', 'fpdf', 'sm1', 'sm2', 'lyanna', 'curvature']
+    for n in [2,3,4,5]:
+        for Sn in combinations(S_list, n):
+            file_name = f"{'_'.join(Sn)}.parquet"
+            file_path = Path(DATA_DIR / "joint_chains" / file_name)
+            if file_path.exists():
+                pass
+            else:
+                print(f"Inferring... {file_name}")
+                chain = inference([*Sn], niter=Niter)
+                chain.to_parquet(DATA_DIR / "joint_chains" / file_name)
 
     
